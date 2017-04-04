d7f666d26a7cbf6d3514c63215d66464cd88efe7
devikad <devikad@DEVIKAD>
2017-04-04 14:41:06 +0530

    Feature1 Commit2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Feature Branch");
+            Console.WriteLine("2nd Commit");
         }
     }
 }
